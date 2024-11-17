6908b537567b937194d29b91b3a6044723c721b8
KyiLwinOo <khant.bmruby@gmail.comkhant.bmruby@gmail.comkhant.bmruby@gmail.com>
2024-11-17 23:33:25 +0700

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,6 @@ class MyApp extends StatelessWidget {
         textTheme: GoogleFonts.openSansTextTheme(Theme.of(context).textTheme)
             .apply(bodyColor: Colors.white,)
             .copyWith(
-          bodyText1: const TextStyle(color: bodyTextColor),
-          bodyText2: const TextStyle(color: bodyTextColor),
         ),
       ),
 
